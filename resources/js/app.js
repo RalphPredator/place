8f2318fcdb1d237d99e6ac84c0679d9b2e8fae06
Foumbi Audrey <foumbi45@gmail.com>
2025-06-09 16:40:42 +0200

    Update Welcome View and add doker-compose

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,9 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Place';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
